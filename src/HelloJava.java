ddf8c662f9dec5842a5e3c1bf88b0c669fc3fce9
gurwl <gurwls247@naver.com>
2024-06-05 18:21:53 +0900

    test hj

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,7 +1,6 @@
 public class HelloJava {
 
     public static void main(String[] args){
-        System.out.println("hello java to kk1khj");
-
+        System.out.println("hello java to hj");
     }
 }
