631521c36d629ce7ec44aa8df152f66ddc8baeea
onepunchkim <hyukjin4711@gmail.com>
2024-06-15 18:25:03 +0900

    Var test

--- src/variable/Var6.java
+++ src/variable/Var6.java
@@ -3,6 +3,6 @@ package variable;
 public class Var6 {
     public static void main(String[] args) {
         int a;
-        //System.out.println(a);
+        //System.out.println(a); //주석을 풀면 컴파일 에러 발생
     }
 }
