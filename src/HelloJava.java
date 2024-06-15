784cdfae3e541b4021c31c7c257c661811a91699
onepunchkim <hyukjin4711@gmail.com>
2024-06-15 16:46:22 +0900

    test0615

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,7 +1,6 @@
 public class HelloJava {
 
     public static void main(String[] args){
-        System.out.println("hello java to k" +
-                "hj");
+        System.out.println("hello java to khjj");
     }
 }
