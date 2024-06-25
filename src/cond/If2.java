40679c1971c80447117533fdb0ad4b091929b252
onepunchkim <hyukjin4711@gmail.com>
2024-06-25 18:09:25 +0900

    con study

--- src/cond/If2.java
+++ src/cond/If2.java
@@ -2,7 +2,7 @@ package cond;
 
 public class If2 {
     public static void main(String[] args) {
-        int age = 20;
+        int age = 19;
 
         if (age >= 18){
             System.out.println("성인입니다.");
