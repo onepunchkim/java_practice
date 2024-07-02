6b65b530d2d881d8d28cce59eb92944b97d83384
onepunchkim <hyukjin4711@gmail.com>
2024-07-02 16:47:40 +0900

    loop study

--- src/loop/DoWhile2.java
+++ src/loop/DoWhile2.java
@@ -1,12 +1,12 @@
 package loop;
 
-public class DoWhile1 {
+public class DoWhile2 {
     public static void main(String[] args) {
         int i = 10;
 
-        while (i <3){
+        do {
             System.out.println("현재 숫자는: " + i);
             i++;
-        }
+        } while (i <3);
     }
 }
