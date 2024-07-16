fd9ca0575368ca5e76c176913bc5882b64fcf869
onepunchkim <hyukjin4711@gmail.com>
2024-07-16 17:20:10 +0900

    array study

--- src/array/ex/ArrayEx5.java
+++ src/array/ex/ArrayEx5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ArrayEx5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("입력 받을 숫자의 개수를 입력하세요 : ");
+        System.out.print("입력받을 숫자의 개수를 입력하세요 : ");
         int count = scanner.nextInt();
 
         int[] numbers = new int[count];
@@ -18,6 +18,7 @@ public class ArrayEx5 {
             sum += numbers[i];
         }
         average = (double) sum / numbers.length;
+
         System.out.println("입력한 정수의 합계 : " + sum);
         System.out.println("입력한 정수의 평균 : " + average);
     }
